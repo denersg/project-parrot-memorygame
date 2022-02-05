389125e6ed44ca1fb6de719ea4de0c1f672cca3b
Dener Silva <dener.ufes@gmail.com>
2022-02-05 13:16:02 -0300

    Adicionado o giro da carta ao clicar

--- scripts/parrots.js
+++ scripts/parrots.js
@@ -1,3 +1,16 @@
+/* ####### ANOTAÇÕES DO CÓDIGO #######
+# 
+# As funções do jogo foram criadas de baixo para cima, sendo as das últimas linhas as primeiras a serem executadas.
+# A lógica das funções funciona assim:
+# 
+# 1. A primeira função a ser chamada cria o esqueleto que vai acomodar a lista de cartas.
+# 2. Ao executar a função que vai comportar as cartas, em seguida é chamada a próxima função que vai validar a escolha do usuário.
+# 3.
+
+  O jogo funciona da seguinte forma:
+  1. 
+*/
+
 // numberOfCards = prompt("Com quantas cartas deseja começar? [escolha números pares entre 4 e 14]");
 let numberOfCardsChosen, numberOfCardsAllowed = false;
 const cards = [
@@ -22,6 +35,19 @@ const cards = [
 //         }
 //     }
 // }
+
+function turnCardFaceUp(pressedCard){
+    /*Se a carta já está virada, quer dizer que não preciso virá-la
+     *novamente. Então, eu saio da função, pois não
+     *faz sentido continuar a virar uma carta já virada.*/
+    const alreadyTurned = pressedCard.classList.contains("upturned-card");
+    // console.log(alreadyTurned)
+    if(alreadyTurned){
+        return;
+    }
+
+    pressedCard.classList.add("upturned-card");
+}
 
 function comparator() { 
 	return Math.random() - 0.5; 
@@ -49,8 +75,9 @@ function buildMemoryGame(){
     // Acessa a lista não ordenada e insere os itens
     boxOfCards.innerHTML = "";
     for(let i = 0; i < cardShown.length; i++){
+        // <li class="card upturned-card"></li>
         boxOfCards.innerHTML += `
-            <li class="card upturned-card">
+            <li class="card" onclick="turnCardFaceUp(this)">
                 <div class="front template">
                     <img src="./images/front.png">
                 </div>
